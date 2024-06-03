0c6372635174416c66992d0ec275fd82e14a19cb
Soundarya3769 <158430218+Soundarya3769@users.noreply.github.com>
2024-06-03 21:35:30 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum install git -y
-sudo git clone https://github.com/Soundarya3769/car-prediction.git
+sudo git clone https://github.com/Soundarya3769/fish.git
 cd /
-cd car-prediction/
+cd fish/
 sudo yum install python3 python3-pip -y
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
