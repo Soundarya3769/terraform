09ef5a8e3ea5533fc28a824f7910fb7a9148e661
Soundarya3769 <158430218+Soundarya3769@users.noreply.github.com>
2024-06-05 20:49:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,103 +1,109 @@
 provider "aws" {
-  region     = "us-east-2"
+  region = "us-east-2"
 }
-#vpc.tf
+
+# VPC
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
+
   tags = {
     Name = "main"
   }
 }
 
-#creating subnet:
+# Subnet
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.subnet_cidr
   availability_zone = "us-east-2a"
+
   tags = {
     Name = "subnet"
   }
 }
-#creating internet gateway
+
+# Internet Gateway
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
-#creating Route table
+# Route Table
 resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
+
   tags = {
     Name = "Route to internet"
   }
 }
 
-#Associating Route table
+# Route Table Association
 resource "aws_route_table_association" "subnet_assoc" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.main.id
 }
-# Creating Security Group
-resource "aws_security_group" "car_prediction_sg" {
-  // Your configuration for the security group
-}
 
+# Security Group
+resource "aws_security_group" "car_prediction_sg" {
   vpc_id = aws_vpc.main.id
-  # Inbound Rules
-  # HTTP access from anywhere
+
+  // Inbound Rules
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # HTTPS access from anywhere
+
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # SSH access from anywhere
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # Outbound Rules
-  # Internet access to anywhere
+
+  // Outbound Rules
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "Web SG"
   }
 }
 
-
-# Defining CIDR Block for VPC
+# Variables
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
-# Defining CIDR Block for Subnet
+
 variable "subnet_cidr" {
   default = "10.0.1.0/24"
 }
-# Creating EC2 instance
+
+# EC2 Instance
 resource "aws_instance" "car_prediction" {
   ami                         = "ami-0ca2e925753ca2fb4"
   instance_type               = "t2.micro"
@@ -107,13 +113,13 @@ resource "aws_instance" "car_prediction" {
   subnet_id                   = aws_subnet.main.id
   associate_public_ip_address = true
   user_data                   = file("userdata.sh")
+
   tags = {
     Name = "car-prediction-Instance"
   }
 }
 
+# Output
 output "public_ip" {
   value = aws_instance.car_prediction[*].public_ip
 }
-
-
